9951127758a6b9ae01ff4e442f03735212594af6
GitHub账号 <3260191057@qq.com>
2022-08-16 16:17:46 +0800

    Changes

---
+++ src/main/java/com/example/exam/entity/FillQuestion.java
@@ -0,0 +1,22 @@
+package com.example.exam.entity;
+
+import lombok.Data;
+
+@Data
+public class FillQuestion {
+    private Integer questionId;
+
+    private String subject;
+
+    private String question;
+
+    private String answer;
+
+    private Integer score;
+
+    private String level;
+
+    private String section;
+
+    private String analysis;
+}
