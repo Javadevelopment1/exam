d8f6fb19e1cb155c08f2ad984ec77a864b33f484
GitHub账号 <3260191057@qq.com>
2022-08-30 08:55:12 +0800

    Changes

---
+++ src/main/java/com/exam/entity/MultiQuestion.java
@@ -0,0 +1,27 @@
+package com.exam.entity;
+
+public class MultiQuestion {
+    private Integer questionId;
+
+    private String subject;
+
+    private String section;
+
+    private String answerA;
+
+    private String answerB;
+
+    private String answerC;
+
+    private String answerD;
+
+    private String question;
+
+    private String level;
+
+    private String rightAnswer;
+
+    private String analysis; //题目解析
+
+    private Integer score;
+}
