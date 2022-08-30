83ac9b3d821a8d06be3a5e9e676e78090fdc2551
GitHub账号 <3260191057@qq.com>
2022-08-30 08:54:21 +0800

    Changes

--- src/main/java/com/example/exam/MybatisPlusConfig.java
+++ src/main/java/com/example/exam/MybatisPlusConfig.java
@@ -1,4 +1,18 @@
 package com.example.exam;
 
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@Configuration
+@MapperScan("com.example.exam.service.*.mapper*")
+
 public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
 }
