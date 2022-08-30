aa875b11908f0d9edd783a740d64cd21826b041e
GitHub账号 <3260191057@qq.com>
2022-08-30 08:49:05 +0800

    Changes

---
+++ src/main/java/com/example/exam/serviceimpl/AnswerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.example.exam.serviceimpl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.example.exam.mapper.AnswerMapper;
+import com.example.exam.service.AnswerService;
+import com.example.exam.vo.AnswerVO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnswerServiceImpl implements AnswerService {
+    @Autowired
+    private AnswerMapper answerMapper;
+
+    @Override
+    public IPage<AnswerVO> findAll(Page<AnswerVO> page) {
+        return answerMapper.findAll(page);
+    }
+}
+
