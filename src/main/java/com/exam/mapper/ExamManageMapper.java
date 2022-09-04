9163078b518d6967cf9e36bd60bfbd897a43a2d9
GitHub账号 <3260191057@qq.com>
2022-09-04 16:41:14 +0800

    Changes

--- src/main/java/com/exam/mapper/ExamManageMapper.java
+++ src/main/java/com/exam/mapper/ExamManageMapper.java
@@ -1,26 +1,25 @@
 package com.exam.mapper;
 
-import com.alibaba.druid.support.http.util.IPRange;
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.exam.entity.ExamManage;
 import org.apache.ibatis.annotations.*;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Mapper
 public interface ExamManageMapper {
-    @Service("select * from exam_manage")
-    List<ExamManageMapper> findAll();
+    @Select("select * from exam_manage")
+    List<ExamManage> findAll();
 
-    @Service("select * from exam_manage")
-    IPRange<ExamManageMapper> findAll(Page page);
+    @Select("select * from exam_manage")
+    IPage<ExamManage> findAll(Page page);
 
-    @Service("select * from exam_manage where examCode = #{examCode}")
-    ExamManageMapper findById(Integer examCode);
+    @Select("select * from exam_manage where examCode = #{examCode}")
+    ExamManage findById(Integer examCode);
 
     @Delete("delete from exam_manage where examCode = #{examCode}")
-    int delete(Integer examCode);
+    int delete(@Param("examCode") Integer examCode);
 
     @Update("update exam_manage set description = #{description},source = #{source},paperId = #{paperId}," +
             "examDate = #{examDate},totalTime = #{totalTime},grade = #{grade},term = #{term}," +
@@ -31,7 +30,7 @@ public interface ExamManageMapper {
     @Options(useGeneratedKeys = true, keyProperty = "examCode")
     @Insert("insert into exam_manage(description,source,paperId,examDate,totalTime,grade,term,major,institute,totalScore,type,tips)" +
             " values(#{description},#{source},#{paperId},#{examDate},#{totalTime},#{grade},#{term},#{major},#{institute},#{totalScore},#{type},#{tips})")
-    int add(@Param("exammanage") ExamManage exammanage);
+    int add(ExamManage exammanage);
 
     /**
      * 查询最后一条记录的paperId,返回给前端达到自增效果
