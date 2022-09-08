d99b87863555af7e260b081d81f15e3d5ef4ea3c
GitHub账号 <3260191057@qq.com>
2022-09-08 16:03:42 +0800

    Changes

--- src/main/java/com/exam/serviceimpl/LoginServiceImpl.java
+++ src/main/java/com/exam/serviceimpl/LoginServiceImpl.java
@@ -6,7 +6,9 @@ import com.exam.entity.Teacher;
 import com.exam.mapper.LoginMapper;
 import com.exam.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class LoginServiceImpl implements LoginService {
 
     @Autowired
