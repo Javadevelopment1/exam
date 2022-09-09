4720e15ab7f67ad3ecbde7fd8ea9a3fcd26dda88
GitHub账号 <3260191057@qq.com>
2022-09-09 15:23:33 +0800

    Changes

--- src/main/java/com/exam/mapper/ReplayMapper.java
+++ src/main/java/com/exam/mapper/ReplayMapper.java
@@ -1,7 +1,28 @@
 package com.exam.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.exam.entity.Replay;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
 
 @Mapper
 public interface ReplayMapper {
+    @Select("select messageId,replayId,replay,replayTime from replay")
+    List<Replay> findAll();
+
+    @Select("select messageId,replayId,replay,replayTime from replay where messageId = #{messageId}")
+    List<Replay> findAllById(Integer messageId);
+
+    @Select("select messageId,replayId,replay,replayTime from replay where messageId = #{messageId}")
+    Replay findById(Integer messageId);
+
+    @Delete("delete from replay where replayId = #{replayId}")
+    int delete(Integer requestId);
+
+    @Update("update replay set title = #{title}, replay = #{replay}, replayTime = #{replayTime} where replayId = #{replayId}")
+    int update(Replay replay);
+
+    @Options(useGeneratedKeys = true, keyProperty = "replayId")
+    @Insert("insert into replay(messageId,replay,replayTime) values(#{messageId}, #{replay},#{replayTime})")
+    int add(Replay replay);
 }
