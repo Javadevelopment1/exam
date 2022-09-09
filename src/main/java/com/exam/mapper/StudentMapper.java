8380bf6be57d05ec214f8f9c83d80d10bc82a656
GitHub账号 <3260191057@qq.com>
2022-09-09 15:41:24 +0800

    Changes

--- src/main/java/com/exam/mapper/StudentMapper.java
+++ src/main/java/com/exam/mapper/StudentMapper.java
@@ -1,7 +1,32 @@
 package com.exam.mapper;
 
-import org.apache.ibatis.annotations.Mapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.exam.entity.Student;
+import org.apache.ibatis.annotations.*;
 
 @Mapper
 public interface StudentMapper {
+    @Select("select * from student")
+    IPage<Student> findAll(Page page);
+
+    @Select("select * from student where studentId = #{studentId}")
+    Student findById(Integer studentId);
+
+    @Delete("delete from student where studentId = #{studentId}")
+    int deleteById(Integer studentId);
+
+    @Update("update student set studentName = #{studentName},grade = #{grade},major = #{major},clazz = #{clazz}," +
+            "institute = #{institute},tel = #{tel},email = #{email},pwd = #{pwd},cardId = #{cardId},sex = #{sex},role = #{role} " +
+            "where studentId = #{studentId}")
+    int update(Student student);
+
+    @Update("update student set pwd = #{pwd} where studentId = #{studentId}")
+    int updatePwd(Student student);
+
+
+    @Options(useGeneratedKeys = true, keyProperty = "studentId")
+    @Insert("insert into student(studentName,grade,major,clazz,institute,tel,email,pwd,cardId,sex,role) values " +
+            "(#{studentName},#{grade},#{major},#{clazz},#{institute},#{tel},#{email},#{pwd},#{cardId},#{sex},#{role})")
+    int add(Student student);
 }
