be01082186b4991212553ec7a44f3d942e810734
GitHub账号 <3260191057@qq.com>
2022-09-18 17:08:51 +0800

    Changes

---
+++ src/main/java/com/exam/controller/ExamManageController.java
@@ -0,0 +1,37 @@
+package com.exam.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.exam.entity.ApiResult;
+import com.exam.entity.ExamManage;
+import com.exam.service.ExamManageService;
+import com.exam.util.ApiResultHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExamManageController {
+
+    @Autowired
+    private ExamManageService examManageService;
+
+    @GetMapping("/exams")
+    public ApiResult findAll() {
+        System.out.println("不分页查询所有试卷");
+        ApiResult apiResult;
+        apiResult = ApiResultHandler.buildApiResult(200, "请求成功！", examManageService.findAll());
+        return apiResult;
+    }
+
+    @GetMapping("/exams/{page}/{size}")
+    public ApiResult findAll(@PathVariable("page") Integer page, @PathVariable("size") Integer size) {
+        System.out.println("分页查询所有试卷");
+        ApiResult apiResult;
+        Page<ExamManage> examManage = new Page<>(page, size);
+        IPage<ExamManage> all = examManageService.findAll(examManage);
+        apiResult = ApiResultHandler.buildApiResult(200, "请求成功！", all);
+        return apiResult;
+    }
+}
