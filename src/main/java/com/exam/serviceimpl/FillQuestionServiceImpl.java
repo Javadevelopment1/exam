6e57914c698a49e2beb8169bffa893639518016c
GitHub账号 <3260191057@qq.com>
2022-09-19 17:23:40 +0800

    Changes

--- src/main/java/com/exam/serviceimpl/FillQuestionServiceImpl.java
+++ src/main/java/com/exam/serviceimpl/FillQuestionServiceImpl.java
@@ -6,9 +6,11 @@ import com.exam.entity.FillQuestion;
 import com.exam.mapper.FillQuestionMapper;
 import com.exam.service.FillQuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class FillQuestionServiceImpl implements FillQuestionService {
 
     @Autowired
